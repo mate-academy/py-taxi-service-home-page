b5e605f00082927929a50b99a3acdaade3b500a1
unknown <arthuromgnh@gmail.com>
2022-04-07 20:11:52 +0300

    solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from taxi_service.settings import TEMPLATES
+
+
+class TemplateTest(TestCase):
+    def test_templates_dir(self):
+        print(TEMPLATES[0]['DIRS'])
+        assert TEMPLATES[0]['DIRS'] == ''
