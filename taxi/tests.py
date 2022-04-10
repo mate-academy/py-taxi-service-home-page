bff8f7abbe5e09537cbeacc7e6ef46e2faf41fac
unknown <arthuromgnh@gmail.com>
2022-04-10 10:06:11 +0300

    Corrected flake8 errors and removed tests

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,10 +1,1 @@
 from django.test import TestCase
-
-
-from taxi_service.settings import TEMPLATES
-
-
-class TemplateTest(TestCase):
-    def test_templates_dir(self):
-        print(TEMPLATES[0]['DIRS'])
-        assert TEMPLATES[0]['DIRS'] == ''
