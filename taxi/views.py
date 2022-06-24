534717e9c610db55da2e79f8a27aee25997864df
Taras Nester <tarasnester414@gmail.com>
2022-06-24 18:20:49 +0300

    created base views

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Driver, Manufacturer, Car
+
+
+def index(request):
+    context = {
+        "title": "Taxi service",
+        "drivers_amount": Driver.objects.count(),
+        "manufacturers_amount": Manufacturer.objects.count(),
+        "cars_amount": Car.objects.count()
+    }
+
+    return render(request, "taxi/index.html", context=context)
