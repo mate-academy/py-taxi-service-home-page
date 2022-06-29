0c396db9cd77c2b65ca413908a30715e11b15018
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-29 20:42:18 +0300

    Prepared all for work with templates. I have done:  taxi/urls.py, views.py, general urls.py in taxi_service and connected this both file; created folder templates , them added taxi/base.html , in settings.py pointed out correct dir to templetes in constant TEMPLATES; and finally dumpdata from last task in json file and load thiadata for this db, Iuse this for test

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from taxi.models import Manufacturer, Driver, Car
 
-# Create your views here.
+
+def index(request):
+    number_drivers = Driver.objects.count()
+    number_manufacturers = Manufacturer.objects.count()
+    number_cars = Car.objects.count()
+
+    context = {
+        "num_drivers" : number_drivers,
+        "num_manufacturers" : number_manufacturers,
+        "num_cars" : number_cars
+    }
+    return render(request=request, template_name="base.html", context=context)
