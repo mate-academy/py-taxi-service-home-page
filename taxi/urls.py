91eab72b9e792be752a8cd3c24f0580dc6ffa200
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-30 00:01:07 +0300

    add templates and static

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from taxi.views import index
+from taxi.views import index, drivers
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("drivers/", drivers, name="drivers")
 ]
 
 app_name = "taxi"
