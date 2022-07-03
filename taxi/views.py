bcb973a90cc1f66299fd218ca97b7b5da5a1ac2e
Elena <lenasharabura@gmail.com>
2022-07-03 16:52:27 +0200

    small changes views and urls

--- taxi/views.py
+++ taxi/views.py
@@ -7,7 +7,10 @@ def index(request):
     amount_of_drivers = Driver.objects.count()
     amount_of_manufactures = Manufacturer.objects.count()
     amount_of_cars = Car.objects.count()
-    context = {"amount_of_drivers": amount_of_drivers, "amount_of_manufactures": amount_of_manufactures,
-               "amount_of_cars": amount_of_cars}
 
+    context = {
+        "amount_of_drivers": amount_of_drivers,
+        "amount_of_manufactures": amount_of_manufactures,
+        "amount_of_cars": amount_of_cars
+    }
     return render(request, "taxi/index.html", context=context)
