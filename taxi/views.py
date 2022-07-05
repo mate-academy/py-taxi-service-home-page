1475275d0dc631fea2f219e4d209988322be2e61
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-07-05 13:04:49 +0300

    Solution 3

---
+++ taxi/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from taxi.models import Driver, Manufacturer, Car
+
+
+def index(request):
+    count_drivers = Driver.objects.count()
+    count_manufacturers = Manufacturer.objects.count()
+    count_cars = Car.objects.count()
+    context = {
+        "count_drivers": count_drivers,
+        "count_manufacturers": count_manufacturers,
+        "count_cars": count_cars,
+    }
+    return render(request, "templates/taxi/index.html", context=context)
