e027aeb01f2a2a0a0ef0c66a27b7d856c3932b17
Vasyl Smutok <23.smutok@gmail.com>
2022-07-11 18:08:58 +0300

    temporary commit

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Driver, Manufacturer, Car
+
+
+def index(requests):
+    count_drivers = Driver.objects.count()
+    count_manufacturers = Manufacturer.objects.count()
+    count_cars = Car.objects.count()
+
+    return render(requests, "taxi/index.html")
