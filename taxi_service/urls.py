ae5b6f20edb598766476058e0ac7461e4cf7b94e
Oleksii Bodnarchuk <olbody12@gmail.com>
2022-09-09 14:38:18 +0300

    solution - fix blank lines

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("taxi/", include("taxi.urls", namespace="taxi"))
+    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
