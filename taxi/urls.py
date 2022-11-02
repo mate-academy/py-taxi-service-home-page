76bd1e00c82808763e0896dca123645679255851
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-02 11:14:59 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from taxi.views import index
 
 
-urlpatterns = [path("", index, name="index")]
+urlpatterns = [
+    path("", index, name="index")
+]
 
 app_name = "taxi"
