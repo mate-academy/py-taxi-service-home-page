756a5b8c48375ac470233b8baa275f8bf7a4f462
Yura <pazyn404@gamil.com>
2022-11-07 19:58:43 +0200

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path("", include("taxi.urls", namespace="taxi")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
