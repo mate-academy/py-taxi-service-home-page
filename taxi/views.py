c8e184e366c38b92e2292f6061eb3eae08272f12
Yurii <pazyn404@gmail.com>
2022-11-09 10:39:02 +0200

    Fix indentation

--- taxi/views.py
+++ taxi/views.py
@@ -7,6 +7,6 @@ def index(request):
     context = {
         "num_drivers": Driver.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
-        "num_cars": Car.objects.count()
+        "num_cars": Car.objects.count(),
     }
     return render(request, "taxi/index.html", context=context)
