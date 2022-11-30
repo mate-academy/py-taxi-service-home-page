0e211086023b8f5f4b892142199232405c44f6d2
George <georgepavlej@gmail.com>
2022-11-30 22:31:50 +0000

    Added number visits of the home page

--- taxi/views.py
+++ taxi/views.py
@@ -7,11 +7,14 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_drivers": num_drivers,
         "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars
+        "num_cars": num_cars,
+        "num_visits": num_visits
     }
 
     return render(request, "taxi/index.html", context=context)
