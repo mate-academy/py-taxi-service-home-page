70fcbea4d79f0f29c324a89a1907fc381adbe7ef
Yulia Bozhieva <bozhieva93@gmail.com>
2022-12-16 16:28:21 -0400

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -4,14 +4,10 @@ from taxi.models import Driver, Manufacturer, Car
 
 
 def index(request):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
-
     context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count()
     }
 
     return render(request, "taxi/index.html", context=context)
