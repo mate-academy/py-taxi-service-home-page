4753fd04f8abb9d91e3e8e94b79b5c11fca5a39c
Anton Chumak <Chumak7377@gmail.com>
2022-12-16 19:19:30 +0200

    decorated home page

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,14 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
 from taxi.models import Driver, Manufacturer, Car
+
+
+def timing():
+    current_date = datetime.now()
+    next_year_start = datetime(day=1, month=1, year=2023)
+    time_delta = next_year_start - current_date
+    return time_delta
 
 
 def index(request):
@@ -11,6 +20,8 @@ def index(request):
         "num_drivers": num_drivers,
         "num_manufacturers": num_manufacturers,
         "num_cars": num_cars,
+        "before_new_year": timing(),
+
     }
 
     return render(request, "taxi/index.html", context=context)
