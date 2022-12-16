e2e219dae4250e482a6fb35983eba900d0c107b3
Anton Chumak <Chumak7377@gmail.com>
2022-12-16 22:35:43 +0200

    solution taxi service home page

--- taxi/views.py
+++ taxi/views.py
@@ -7,21 +7,14 @@ from taxi.models import Driver, Manufacturer, Car
 def timing():
     current_date = datetime.now()
     next_year_start = datetime(day=1, month=1, year=2023)
-    time_delta = next_year_start - current_date
-    return time_delta
+    return next_year_start - current_date
 
 
 def index(request):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
-
     context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars,
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count(),
         "before_new_year": timing(),
-
     }
-
     return render(request, "taxi/index.html", context=context)
