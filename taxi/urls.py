52be970599e2841c87b0b1bbf025fa67600320fe
Paul Starodub <amorallex@gmail.com>
2022-12-31 00:44:58 +0400

    Taxi with right path

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,11 +1,9 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from taxi.views import index
 
 urlpatterns = [
     path("", index, name="index")
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 app_name = "taxi"
