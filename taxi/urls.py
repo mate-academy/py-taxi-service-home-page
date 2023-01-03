b67ed26d7555006250768c355f78632fe23884de
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-01-03 19:19:04 +0200

    solution1

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from taxi.views import index
 
 urlpatterns = [
-    path('', index, name="index")
+    path("", index, name="index")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "taxi"
