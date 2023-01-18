b5c97d2bb771b1e59afdf0377b61a7761e40768f
Daria Hannochenko <soloha7965@gmail.com>
2023-01-18 14:34:15 +0200

    Impoved Solution 1.1

--- taxi/views.py
+++ taxi/views.py
@@ -8,8 +8,8 @@ def index(request):
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
     content = {
-            "num_drivers": num_drivers,
-            "num_manufacturers": num_manufacturers,
-            "num_cars": num_cars,
-            }
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
+    }
     return render(request, "taxi/index.html", context=content)
