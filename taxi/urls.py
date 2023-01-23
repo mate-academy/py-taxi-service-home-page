0dcdcc82252960650a16f8510980fcaa18443e59
Ivan Mayor <maramorosh2@gmail.com>
2023-01-23 18:56:22 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from taxi import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
 ]
 
-app_name = 'taxi'
+app_name = "taxi"
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('taxi.urls', namespace='taxi'))
+    path("", include("taxi.urls", namespace="taxi"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
