3042f4a4bfd454edfa7c34f2416bdd68f9fdca6b
Anna Kabatova <kabatova.anna@ukr.net>
2023-02-15 20:00:02 +0200

    resolved flake8 error

--- taxi/views.py
+++ taxi/views.py
@@ -6,10 +6,10 @@ from taxi.models import Manufacturer, Car
 
 def index(request):
 
-	context = {
-		"num_drivers": get_user_model().objects.count(),
-		"num_manufacturers": Manufacturer.objects.count(),
-		"num_cars": Car.objects.count(),
-	}
+    context = {
+        "num_drivers": get_user_model().objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count(),
+    }
 
-	return render(request, "taxi/index.html", context=context)
+    return render(request, "taxi/index.html", context=context)
