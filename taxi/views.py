30aac66c73893d2ca1d255592d783ffdd5312d9b
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-03-20 20:54:04 +0200

    Added css from bootstrap

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from taxi.models import Driver, Manufacturer, Car
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
