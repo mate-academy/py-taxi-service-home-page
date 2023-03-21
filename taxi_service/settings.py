899bde71c7483c408aee41969ec9a37eaf6f0043
Paul Savchuk <nekrjkeee@gmail.com>
2023-03-21 15:54:23 +0200

    added path.join to establish same styling in settings

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "taxi_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
