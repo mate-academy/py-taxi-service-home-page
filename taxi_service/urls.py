335a5a75b61eaef5f41aec40b27256150bc7da73
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-03-28 13:04:46 +0300

    Solution_fixed

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("", include("taxi.urls", namespace="taxi")),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
