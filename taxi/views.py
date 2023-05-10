4ffe9ee6bb0084b341419b2df7bf59ce2698474f
ZaikaDaria <corplinebusiness@gmail.com>
2023-05-11 00:10:57 +0300

    Add solution

--- taxi/views.py
+++ taxi/views.py
@@ -9,9 +9,9 @@ def index(request):
     num_cars = Car.objects.all().count()
 
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
 
     return render(request, "taxi/index.html", context=context)
