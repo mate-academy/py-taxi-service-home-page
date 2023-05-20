9b85b552a0688e1cce05ad0ad1746b6e4fddc49b
Andrew.H <john.riderr9@gmail.com>
2023-05-20 05:44:31 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "taxi_service.urls"
 
+import os
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR / "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
