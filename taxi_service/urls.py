85c6df5d2b85371994958377444ff291b7c5c594
OleksandrLeliukh <alexander.leluh@gmail.com>
2023-05-30 17:04:14 +0300

    'Solution'

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -22,4 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
