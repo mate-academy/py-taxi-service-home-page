9588f8280318d0ef572dfc20ca44ed3cff641c0d
саша1 <magazynuniversam@gmail.com>
2023-06-14 16:28:11 +0300

    solution 3

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path("admin/", admin.site.urls),
-                  path("", include("taxi.urls", namespace="taxi"))
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("admin/", admin.site.urls),
+    path("", include("taxi.urls", namespace="taxi"))
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT)
