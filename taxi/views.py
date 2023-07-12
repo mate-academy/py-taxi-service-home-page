719c4edef755c959fa22ac3c96defb2953459b16
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-12 22:06:52 +0300

    Solution for Taxi Service Home Page

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Car, Driver, Manufacturer
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "num_cars": Car.objects.all().count(),
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count()
+    }
+    return render(request, "taxi/index.html", context=context)
