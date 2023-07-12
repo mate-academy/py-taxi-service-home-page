373997e9cc9e0537ef9cc6e3ebd35579f0565d7b
Mariia Horbova <gorbova21@gmail.com>
2023-07-12 19:08:05 +0300

    flake8

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -3,9 +3,9 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
