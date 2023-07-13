000abf4f450a2c0ec348c33e602f56780e3a6714
YOUR_NAME <YOUR_EMAIL>
2023-07-13 14:20:25 +0300

    Use additional variables in contex.

--- taxi/views.py
+++ taxi/views.py
@@ -4,11 +4,12 @@ from .models import Driver, Manufacturer, Car
 
 
 def index(request):
-
-    context = {
-        "num_drivers": Driver.objects.all().count(),
-        "num_manufacturers": Manufacturer.objects.all().count(),
-        "num_cars": Car.objects.all().count()
-    }
-
-    return render(request, "taxi/index.html", context=context)
+    return render(
+        request,
+        "taxi/index.html",
+        context={
+            "num_drivers": Driver.objects.all().count(),
+            "num_manufacturers": Manufacturer.objects.all().count(),
+            "num_cars": Car.objects.all().count()
+        }
+    )
