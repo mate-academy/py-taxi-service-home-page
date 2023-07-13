01b4e884e36a68c8556f7d589977e7dbc58dc307
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-13 15:22:56 +0300

    Unnecessary all method is removed

--- taxi/views.py
+++ taxi/views.py
@@ -6,7 +6,7 @@ from taxi.models import Car, Driver, Manufacturer
 
 def index(request: HttpRequest) -> HttpResponse:
     context = {
-        "num_cars": Car.objects.all().count(),
+        "num_cars": Car.objects.count(),
         "num_drivers": Driver.objects.count(),
         "num_manufacturers": Manufacturer.objects.count()
     }
