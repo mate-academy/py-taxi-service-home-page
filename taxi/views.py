adda85abaaa3bf23b7b7912459485215e19e6fb9
YOUR_NAME <YOUR_EMAIL>
2023-07-13 14:10:12 +0300

    Remove additional variables in "index"

--- taxi/views.py
+++ taxi/views.py
@@ -4,14 +4,11 @@ from .models import Driver, Manufacturer, Car
 
 
 def index(request):
-    num_drivers = Driver.objects.all().count()
-    num_manufacturers = Manufacturer.objects.all().count()
-    num_cars = Car.objects.all().count()
 
     context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars
+        "num_drivers": Driver.objects.all().count(),
+        "num_manufacturers": Manufacturer.objects.all().count(),
+        "num_cars": Car.objects.all().count()
     }
 
     return render(request, "taxi/index.html", context=context)
