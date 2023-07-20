7ce9fc076d6b184aa1e11f4d33bb61c922bd2596
stepband <vladshvik@gmail.com>
2023-07-20 11:12:43 +0300

    Added basic function of taxi service

--- taxi/models.py
+++ taxi/models.py
@@ -17,5 +17,7 @@ class Driver(AbstractUser):
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
