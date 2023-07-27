584162a0834a3a05765c1288598fec24c7251f2a
S-SMTN <cven28@gmail.com>
2023-07-28 01:28:31 +0300

    implemented urls and templates

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from taxi_service.settings import AUTH_USER_MODEL
 
 
 class Manufacturer(models.Model):
@@ -18,4 +19,4 @@ class Driver(AbstractUser):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver, related_name="cars")
+    drivers = models.ManyToManyField(AUTH_USER_MODEL, related_name="cars")
--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,19 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Driver, Manufacturer, Car
+
+
+def index(request: WSGIRequest) -> HttpResponse:
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars
+    }
+
+    return render(request, "taxi/index.html", context=context)
