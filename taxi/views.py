3180e76d8181aa83005d692acf4f29021b1a9615
Oleksandr Ilchenko <oleksandrilchenko@Air-Oleksandr.lan>
2023-08-25 17:23:18 +0100

    Updated index view

--- taxi/views.py
+++ taxi/views.py
@@ -3,13 +3,10 @@ from taxi.models import Driver, Manufacturer, Car
 
 
 def index(request):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
     context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count()
     }
     print(context)
     return render(request, "taxi/index.html", context=context)
