24b512c0c6e030251744fb30db5780ffee673be4
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-09-20 15:21:53 +0300

    Implemented MTV app with View according to the task.

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Manufacturer, Driver, Car
+
+
+def index(request: HttpRequest) -> object:
+    num_drivers = Driver.objects.all().count()
+    num_manufacturers = Manufacturer.objects.all().count()
+    num_cars = Car.objects.all().count()
+    return render(
+        request,
+        "taxi/index.html",
+        context={
+            "num_drivers": num_drivers,
+            "num_manufacturers": num_manufacturers,
+            "num_cars": num_cars,
+        },
+    )
