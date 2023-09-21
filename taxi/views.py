4df56e6bb5ad5013d1d549127a28012ac80576e9
Oleksandr Moroz <oleksandrmoroz2003@gmail.com>
2023-09-21 17:49:49 +0300

    fix taxi views and add css

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
+
 from taxi.models import Manufacturer, Driver, Car
 
 
 def index(request):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
+
     context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count()
     }
 
     return render(
