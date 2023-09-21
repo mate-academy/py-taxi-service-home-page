189f1134cd5dec02b6fbcad2f0ffc43c6bdfee96
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-09-21 21:17:57 +0300

    fixed indentation

--- taxi/views.py
+++ taxi/views.py
@@ -5,10 +5,11 @@ from taxi.models import Car, Driver, Manufacturer
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request,
-                  "taxi/index.html",
-                  context={
-                      "num_drivers": Driver.objects.count(),
-                      "num_cars": Car.objects.count(),
-                      "num_manufacturers": Manufacturer.objects.count()
-                  })
+    return render(
+        request,
+        "taxi/index.html",
+        context={
+            "num_drivers": Driver.objects.count(),
+            "num_cars": Car.objects.count(),
+            "num_manufacturers": Manufacturer.objects.count()
+        })
