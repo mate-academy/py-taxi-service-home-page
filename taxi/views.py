dc288a4ae43bf2b1b22e041327d8ca825bf6a9ec
anna-lybid <anna.lybid@gmail.com>
2023-09-21 20:29:07 +0300

    Update

--- taxi/views.py
+++ taxi/views.py
@@ -5,12 +5,9 @@ from taxi.models import Driver, Manufacturer, Car
 
 
 def index(request: HttpRequest):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
     context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers
+        "num_drivers": Driver.objects.count(),
+        "num_cars": Car.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count()
     }
     return render(request, "taxi/index.html", context=context)
