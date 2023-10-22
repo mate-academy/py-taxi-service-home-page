3da7414cae23124967faab4f9d9b59d5b3fefe69
Anastasia <anastasiiya.p@gmail.com>
2023-10-22 13:49:44 +0300

    Fixed

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -125,10 +125,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "py-taxi-service-home-page/taxi/static",
-# ]
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
