c7fe6b074b3620611182f7512a2f938c521d4c98
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-11-09 18:41:20 +0200

    deleted empty lines

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,7 +3,5 @@ from .views import index
 
 urlpatterns = [
     path("", index, name="index")
-
 ]
-
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -11,6 +11,5 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_drivers": num_drivers,
         "num_manufacturers": num_manufacturers,
         "num_cars": num_cars
-
     }
     return render(request, "taxi/index.html", context=context)
