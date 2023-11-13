8d3c5115e8491b38a5a208be04ddcfcc8ed883b0
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-11-13 20:52:54 +0200

    Fixed pep8 erorrs

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-app_name = 'taxi_service'
+app_name = "taxi_service"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
