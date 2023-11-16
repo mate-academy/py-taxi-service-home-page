c7cbb754689161fa3037e4d26b321e41d3c02455
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-11-17 00:53:49 +0200

    flake8 fix 2

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -87,22 +87,14 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation"
-        ".UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation" 
-                ".MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation" 
-                ".CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation" 
-                ".NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation"
+             ".UserAttributeSimilarityValidator", },
+    {"NAME": ("django.contrib.auth.password_validation"
+              ".MinimumLengthValidator"), },
+    {"NAME": ("django.contrib.auth.password_validation"
+              ".CommonPasswordValidator"), },
+    {"NAME": ("django.contrib.auth.password_validation"
+              ".NumericPasswordValidator"), },
 ]
 
 AUTH_USER_MODEL = "taxi.Driver"
