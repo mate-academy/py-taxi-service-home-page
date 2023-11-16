cfde22811ed965e5c97df28cc2b12160bd6ae86c
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-11-16 13:37:08 +0200

    Fixed namaspace

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxis"))
+    path("", include("taxi.urls", namespace="taxi"))
 ]
