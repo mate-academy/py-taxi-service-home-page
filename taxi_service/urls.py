995a9f20d1c7898f23a4e52eafc5234089a6042c
nsdNite <nsd7night@gmail.com>
2023-11-16 14:55:26 +0200

    Solution taxi-service-home-page

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path("admin/", admin.site.urls),
-                  path("taxi/", include("taxi.urls", namespace="taxi"))
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("admin/", admin.site.urls),
+    path("taxi/", include("taxi.urls", namespace="taxi"))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
