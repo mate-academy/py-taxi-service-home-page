545c4c1fa0ed96ed7feaddbfc9c3eb7943f6e314
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-11-23 14:16:23 +0200

    Add annotation for request

--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 from taxi.models import Driver, Manufacturer, Car
 
 
-def index(request) -> HttpResponse:
+def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
