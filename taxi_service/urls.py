d282c522bc5ab25f34173e9fb12e0ded548002d7
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-12-08 22:36:39 +0200

    import remove

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from taxi.views import *
-
 urlpatterns = [
     path("", include("taxi.urls", namespace="taxi")),  # 1
     path("admin/", admin.site.urls),
