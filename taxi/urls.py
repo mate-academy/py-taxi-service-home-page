7ac8b3a000758c980d380b06676b85b4be5484f7
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-12-08 22:47:11 +0200

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from taxi.views import index
 
+app_name = "taxi"
+
 urlpatterns = [  # 2
     path("", index, name="index"),
 ]
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from taxi.views import index
+
 urlpatterns = [
     path("", include("taxi.urls", namespace="taxi")),  # 1
     path("admin/", admin.site.urls),
