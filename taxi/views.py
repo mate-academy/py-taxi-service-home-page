2291652df74788fea9419bd6e8938aa2af4ebd63
Ruslan <rusipbox@gmail.com>
2023-12-29 19:03:09 +0200

    Test2(py-service-home-page)

--- taxi/views.py
+++ taxi/views.py
@@ -8,8 +8,9 @@ from taxi.models import Driver, Manufacturer, Car
 def index(
         request: HttpRequest
 ) -> HttpResponse:
-    context = {"num_drivers": Driver.objects.count(),
-               "num_manufacturers": Manufacturer.objects.count(),
-               "num_cars": Car.objects.count()
-               }
+    context = {
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count()
+    }
     return render(request, "taxi/index.html", context=context)
