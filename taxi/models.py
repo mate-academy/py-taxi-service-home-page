d89d2ca9276a04a59cae21bf87eeac6000ae2c70
Ruslan <rusipbox@gmail.com>
2023-12-29 21:18:21 +0200

    Test3(py-service-home-page) with default models

--- taxi/models.py
+++ taxi/models.py
@@ -1,45 +1,20 @@
-from django.conf import settings
+from django.db import models
 
 from django.contrib.auth.models import AbstractUser
-
-from django.db import models
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
-    class Meta:
-        ordering = ("name", )
-
-    def __str__(self):
-        return f"{self.name} ({self.country})"
-
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, blank=False, null=False)
-
-    class Meta:
-        ordering = ("username", )
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.username})"
+    license_number = models.CharField(max_length=255, unique=True)
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(
-        Manufacturer,
-        on_delete=models.CASCADE,
-        related_name="cars"
+        Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
-    drivers = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        related_name="car"
-    )
-
-    class Meta:
-        ordering = ("model", )
-
-    def __str__(self):
-        return f"{self.model} ({self.manufacturer})"
+    drivers = models.ManyToManyField(Driver, related_name="cars")
