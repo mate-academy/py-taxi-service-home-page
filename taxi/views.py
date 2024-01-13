95c9e99e4cf799356b9aa44390795b40d428ec94
MykolaMazhula <mykm3ua@gmail.com>
2024-01-13 23:52:30 +0100

    edited

--- taxi/views.py
+++ taxi/views.py
@@ -9,8 +9,12 @@ def index(request) -> HttpResponse:
     num_drivers = get_user_model().objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
-    return render(request,
-                  "taxi/index.html",
-                  {"num_drivers" : num_drivers,
-                   "num_manufacturers" : num_manufacturers,
-                   "num_cars" : num_cars})
+    return render(
+        request,
+        "taxi/index.html",
+        {
+            "num_drivers" : num_drivers,
+            "num_manufacturers" : num_manufacturers,
+            "num_cars" : num_cars
+        }
+    )
