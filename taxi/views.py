412d155f3cf6ccc3ba6cbe6057e9dc1e04f80d09
MykolaMazhula <mykm3ua@gmail.com>
2024-01-13 13:40:50 +0100

    Taxi-Home-Page

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .models import Manufacturer, Car
+
+
+def index(request) -> HttpResponse:
+    num_drivers = get_user_model().objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+    return render(request,
+                  "taxi/index.html",
+                  {"num_drivers" : num_drivers,
+                   "num_manufacturers" : num_manufacturers,
+                   "num_cars" : num_cars})
