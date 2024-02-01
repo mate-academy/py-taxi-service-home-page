af85ffe200aa0ebfd8d56baa2fad451727802593
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-02-01 13:15:48 +0200

    edited files

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from taxi.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
