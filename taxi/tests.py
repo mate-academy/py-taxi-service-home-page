88fbb07de3639a3576502d0087931457bceb12ac
Sikach Illia <sikatch.ilja@gmail.com>
2024-02-25 00:45:33 +0200

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from taxi.models import Manufacturer, Driver, Car
+
+
+class HomeTestPage(TestCase):
+    def setUp(self):
+        for _ in range(5):
+            Driver.objects.create(username="test" + str(_),
+                                  license_number=str(_))
+        for _ in range(10):
+            Car.objects.create(model="test", manufacturer_id=1)
+        for _ in range(16):
+            Manufacturer.objects.create(name="test" + str(_))
+
+    def test_context_data(self):
+        url = reverse("taxi:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["num_drivers"], Driver.objects.count())
+        self.assertEqual(response.context["num_manufacturers"], Manufacturer.objects.count())
+        self.assertEqual(response.context["num_cars"], Car.objects.count())
