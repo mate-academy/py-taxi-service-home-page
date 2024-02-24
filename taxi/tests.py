6fa19ae176aa468ddbedda882e16e3f57b18f34b
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-02-25 00:49:39 +0200

    Update tests.py

--- taxi/tests.py
+++ taxi/tests.py
@@ -17,7 +17,12 @@ class HomeTestPage(TestCase):
     def test_context_data(self):
         url = reverse("taxi:index")
         response = self.client.get(url)
+        
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["num_drivers"], Driver.objects.count())
-        self.assertEqual(response.context["num_manufacturers"], Manufacturer.objects.count())
-        self.assertEqual(response.context["num_cars"], Car.objects.count())
+        
+        self.assertEqual(response.context["num_drivers"], 
+                         Driver.objects.count())
+        self.assertEqual(response.context["num_manufacturers"], 
+                         Manufacturer.objects.count())
+        self.assertEqual(response.context["num_cars"], 
+                         Car.objects.count())
