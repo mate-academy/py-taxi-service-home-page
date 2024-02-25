e229e13373b64dc2bff6802f6a4ac54a9bf3ed24
Artem Chernyshov <achernyshovv1@gmail.com>
2024-02-25 15:07:26 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR/"templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR/"static"
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
