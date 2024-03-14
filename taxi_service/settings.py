5aeee9229f04a0670dc0e833c9c4e0342130126e
Illya Maznitskiy <comercaleuros@gmail.com>
2024-03-14 16:26:23 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
