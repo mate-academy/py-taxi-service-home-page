312d1e64e3674a6a079391ab0312f0dcc0b95c28
bujhm <Bujhm99@gmail.com>
2024-04-13 12:45:43 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": ["template"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
