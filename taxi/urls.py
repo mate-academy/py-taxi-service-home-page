57680faeff83477eb15766b880edb8e4d8527f5e
xboris <boris.tikhonov.21@gmail.com>
2024-04-14 23:00:40 +0300

    'solution'

---
+++ taxi/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.static import static
+from django.urls import path
+
+from taxi_service import settings
+from taxi.views import index
+
+urlpatterns = [
+                  path('', index, name='index'),
+              ] + static(settings.STATIC_URL)
+
+app_name = 'taxi'
