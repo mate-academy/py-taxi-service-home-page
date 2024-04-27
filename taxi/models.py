b55a0dfcd175fe03903286bcc220e56084181568
Alisa <alisasklrv@gmail.com>
2024-04-27 17:01:00 +0300

    solution

--- taxi/models.py
+++ taxi/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
--- taxi/views.py
+++ taxi/views.py
@@ -8,8 +8,8 @@ def index(request: HttpRequest) -> HttpResponse:
     num_manufacturers = Manufacturer.objects.count()
     num_drivers = Driver.objects.count()
     context = {
-        'num_cars': num_cars,
-        'num_manufacturers': num_manufacturers,
-        'num_drivers': num_drivers
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
+        "num_drivers": num_drivers
     }
     return render(request, "taxi/index.html", context=context)
