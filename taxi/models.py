90f5dcc03ed1e177558b575215fb9e78ea55ccf1
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-04 19:01:47 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -6,9 +6,15 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
 class Car(models.Model):
@@ -17,3 +23,6 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def __str__(self):
+        return f"{self.model} ({self.manufacturer})"
