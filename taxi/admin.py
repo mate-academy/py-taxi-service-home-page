decba1da3d16a49febdfc18039b4e9b1872633a4
bujhm <Bujhm99@gmail.com>
2024-05-09 02:56:14 +0300

    add blank lines, change import

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-from .models import Driver, Car, Manufacturer
 from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Driver, Car, Manufacturer
+
 
 
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = (UserAdmin.fieldsets
                  + (("Additional info", {"fields": ("license_number",)}),))
-    for_flake = ("first_name", "last_name", "license_number",)
     add_fieldsets = (UserAdmin.add_fieldsets
                      + (("Additional info",
-                         {"fields": for_flake}),))
+                         {"fields": (
+                             "first_name", "last_name", "license_number",
+                         )}),))
 
 
 class CarAdmin(admin.ModelAdmin):
