78bf50e4c886cd83432e51d54f19e1974580f3e5
semermaks <semermaks@mail.com>
2024-05-09 20:06:12 +0300

    return type added, comments deleted

--- taxi/views.py
+++ taxi/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 from taxi.models import Driver, Manufacturer, Car
 
 
-# Create your views here.
-def index(request: HttpRequest):
+def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
