ea1951b79c52689d75359d8b5a82dc4529fd66ac
Githi54 <chinter.games1@gmail.com>
2024-05-27 21:54:53 +0300

    basic setup

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Driver, Manufacturer, Car
+
 def index(request):
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+
     return HttpResponse("<h1>Hello world</h1>")
