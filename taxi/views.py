0f6aad19241533dc9217fbd5ad5cf7ecde828acf
Olha Rozdorozhna <olga.rozdorozhna@gmail.com>
2024-05-29 15:41:58 +0300

    Merge pull request #428 from Serhii-Leonenko/refactoring
    fixed models and readme

--- taxi/views.py
+++ taxi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .models import Driver, Manufacturer, Car
 
+
 def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
@@ -14,4 +15,3 @@ def index(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, "taxi/index.html", context)
-
