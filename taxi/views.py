76342f9302a03fab9beb074bf8405827cc1a648d
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-07-08 22:31:03 +0200

    Solve

--- taxi/views.py
+++ taxi/views.py
@@ -5,8 +5,8 @@ from .models import Driver, Manufacturer, Car
 
 def index(request: HttpRequest):
     context = {
-        'num_drivers': Driver.objects.count(),
-        'num_manufacturers': Manufacturer.objects.count(),
-        'num_cars': Car.objects.count()
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count(),
     }
     return render(request, "taxi/index.html", context=context)
