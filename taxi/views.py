6d965ee3fd79d8f97298109634998fce22b71357
Vlad <vlad@example.com>
2024-07-17 18:58:38 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from taxi.models import Car, Manufacturer, Driver
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
 
 
 # Create your views here.
