d11719053410ca377f5464893ba9199f6eaa7353
Sofiia Zholud <szholuds@gmail.com>
2024-08-14 21:40:10 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Driver, Manufacturer, Car
 
 
-def index(request:HttpRequest) -> HttpResponse:
+def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
