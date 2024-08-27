dc4b2936d172ddaf5e4eeedc51c02f67e46bbe8e
Liliya <liliyakhu@ukr.net>
2024-08-27 19:35:43 +0300

    improve code

--- taxi/views.py
+++ taxi/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-from .models import Driver, Car, Manufacturer
+from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request: HttpRequest) -> HttpResponse:
