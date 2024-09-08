37806f11ca3e4bf4a40e9175defcd39d9f2ec066
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-08 21:29:04 +0300

    My beautiful page fixed flake8

--- taxi/views.py
+++ taxi/views.py
@@ -6,9 +6,9 @@ from .models import Driver, Manufacturer, Car
 
 def index(request: HttpRequest) -> HttpResponse:
     context = {
-    "num_drivers" : Driver.objects.count(),
-    "num_manufacturers" : Manufacturer.objects.count(),
-    "num_cars" : Car.objects.count()
+        "num_drivers" : Driver.objects.count(),
+        "num_manufacturers" : Manufacturer.objects.count(),
+        "num_cars" : Car.objects.count()
     }
 
     return render(request, "taxi/index.html", context=context)
