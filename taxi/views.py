825b186b92c621996e28eb37c5333ecdae80eaaf
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-09-09 14:56:56 +0300

    first

--- taxi/views.py
+++ taxi/views.py
@@ -1,22 +1,22 @@
 from django.http import HttpRequest, HttpResponse
-
-from taxi.models import Car, Manufacturer, Driver
-
 from django.shortcuts import render
 
-
-MENU = ["Home page", "Manufacturers", "Cars", "Drivers"]
-EMPTY_MENU = {item: "" for item in MENU}
+from taxi.models import Car, Manufacturer, Driver
 
 
 def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
+
+    menu = {
+        item: "#" for item in ["Home page", "Manufacturers", "Cars", "Drivers"]
+    }
+
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
-        "menu": EMPTY_MENU,
+        "menu": menu,
     }
     return render(request, "taxi/index.html", context=context)
