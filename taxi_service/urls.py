8388cd84c85c4a5c0b3e8f387b2cca0543e6b0f2
Alexandr Politov <apolitov83@gmail.com>
2024-09-09 14:05:47 +0300

    namespace='taxe'

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="index")),
+    path("", include("taxi.urls", namespace="taxi")),
 ]
