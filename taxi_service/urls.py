809cefa0b71150c5a2895c7417e3a28323b9f1a3
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-09 15:11:35 +0300

    second_try

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path("", include("taxi.urls", namespace="taxi")),
 ]
