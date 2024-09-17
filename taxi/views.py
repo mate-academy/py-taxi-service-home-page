3e954800e9d396fe362e8ab581a7d3773eeea507
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-09-17 17:12:04 +0300

    [fix] No reason create this variables, place values diractly to the returned dict

--- taxi/views.py
+++ taxi/views.py
@@ -5,15 +5,12 @@ from taxi.models import Driver, Manufacturer, Car
 
 # Create your views here.
 def index(request):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
     return render(
         request,
         context={
-            "num_drivers": num_drivers,
-            "num_manufacturers": num_manufacturers,
-            "num_cars": num_cars
+            "num_drivers": Driver.objects.count(),
+            "num_manufacturers": Driver.objects.count(),
+            "num_cars": Car.objects.count()
         },
         template_name="taxi/index.html"
     )
