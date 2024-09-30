6247ea2f96ee9b9dd5c32944f4b05c242e30420b
evgenijnozdrenko <evgennozdrenko@MacBook-Pro.local>
2024-09-30 13:57:43 +0300

    Solution

---
+++ taxi/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from taxi.views import index
+
+app_name = 'taxi'  # Простір імен додатка
+urlpatterns = [
+    path('', index, name='index'),
+]
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -124,7 +124,24 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # Шлях до папки з шаблонами
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        }
+    },
+]
