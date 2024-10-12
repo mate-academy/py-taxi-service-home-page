226c2c4614bbe9b7a5a2ccaf5bd2f626cf74ca87
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-10-12 19:07:17 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import Driver, Manufacturer, Car
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_drivers = Driver.objects.count()
+    num_cars = Car.objects.count()
+    num_Manufacturers = Manufacturer.objects.count()
+
+    context = {
+    "num_drivers": num_drivers,
+    "num_cars": num_cars,
+    "num_Manufacturers": num_Manufacturers,
+    }
+
+    return render (request, "taxi/index.html", context=context)
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,10 +54,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "taxi_service.urls"
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "taxi/../templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -79,6 +82,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "DIRS": [BASE_DIR / "templates"],
     }
 }
 
@@ -123,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
