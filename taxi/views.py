4b8dd8c013679a3a8f990e6fc7f917db154b7fa7
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-10-13 13:09:33 +0300

    flake8 fixed

--- taxi/views.py
+++ taxi/views.py
@@ -9,9 +9,9 @@ def index(request: HttpRequest) -> HttpResponse:
     num_manufacturers = Manufacturer.objects.count()
 
     context = {
-    "num_drivers": num_drivers,
-    "num_cars": num_cars,
-    "num_manufacturers": num_manufacturers,
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
     }
 
-    return render (request, "taxi/index.html", context=context)
+    return render(request, "taxi/index.html", context=context)
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8ovil3xu6=eaoqd#" \
+SECRET_KEY = "django-insecure-8ovil3xu6=eaoqd#"\
              "-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -91,7 +91,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".UserAttributeSimilarityValidator",
+        ".UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
