4515bac2ac1573ee09c59fadd387b592ae0dea15
Frezworx <frezworx@gmail.com>
2024-10-15 15:17:33 +0300

    Set default value for SECRET_KEY
    Added a default value to the SECRET_KEY configuration to prevent issues in environments where the secret key is not set. This change ensures better stability and fallback for the application configuration.

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from email.policy import default
 from pathlib import Path
 from decouple import config
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="lkjfhdslfjslagf")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
