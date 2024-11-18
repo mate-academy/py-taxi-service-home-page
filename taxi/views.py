8ea02f0747b400c7dc00e9726acb3e806fe190a5
Yulia Pylypets <yulia250603@gmail.com>
2024-11-18 16:43:21 +0200

    Solution_1

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,13 @@
+from .models import Driver, Manufacturer, Car
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+
+    return render(request, "taxi/index.html", {
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
+    })
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from taxi import views
+
+app_name = "taxi_service"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path("", views.index, name="home"),
 ]
