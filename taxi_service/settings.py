f73af276bc5cbfd47e80363429dba55b66c9c5b5
Dmitry <dimon032006@gmail.com>
2024-11-18 20:29:56 +0100

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = 'taxi.Driver'
+AUTH_USER_MODEL = "taxi.Driver"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
