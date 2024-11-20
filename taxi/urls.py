f5a8fddf69e7377ef8acd7fe94b55fbce44122d1
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-11-20 20:50:00 +0200

    Solution-3.1

--- taxi/urls.py
+++ taxi/urls.py
@@ -5,7 +5,8 @@ app_name = "taxi"  # ключове слово
 
 urlpatterns = [
     path("", views.index, name="index"),  # домашня сторінка
-    path("manufacturers/", views.ManufacturerListView.as_view(), name="manufacturer_list"),
+    path("manufacturers/", views.ManufacturerListView.as_view(),
+         name="manufacturer_list"),
     path("cars/", views.CarListView.as_view(), name="car_list"),
     path("drivers/", views.DriverListView.as_view(), name="driver_list"),
 ]
