a1552387ed2ac0681fa714f5e6b86f6043e1268b
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-11-20 20:47:32 +0200

    Solution-3

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from taxi.models import Driver, Manufacturer, Car
 
@@ -15,3 +16,18 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", data_to_template)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+
+
+class CarListView(ListView):
+    model = Car
+    template_name = "taxi/car_list.html"
+
+
+class DriverListView(ListView):
+    model = Driver
+    template_name = "taxi/driver_list.html"
