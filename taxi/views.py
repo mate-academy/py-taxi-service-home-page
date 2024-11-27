1843e4ccca013871e50b03cc066f598b535a4e3c
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2024-11-27 22:37:21 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .models import Manufacturer, Driver, Car
 
+
 def index(request: HttpRequest) -> HttpResponse:
     context = {
         "num_drivers": Driver.objects.count(),
