bea96b370d74f2539c4c2c538ae08c7679caee79
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-12-05 20:40:52 +0100

    fixed 4

--- taxi/views.py
+++ taxi/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
 from .models import Driver, Manufacturer, Car
 
+
 def index(request):
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
-    return render(request, 'taxi/index.html', context)
+    return render(request, "taxi/index.html", context)
+
 
 def manufacturers(request):
     manufacturers = Manufacturer.objects.all()
-    return render(request, 'taxi/manufacturers.html', {'manufacturers': manufacturers})
+    return render(request, "taxi/manufacturers.html",
+                  {"manufacturers": manufacturers})
+
 
 def cars(request):
     cars = Car.objects.all()
-    return render(request, 'taxi/cars.html', {'cars': cars})
+    return render(request, "taxi/cars.html",
+                  {"cars": cars})
+
 
 def drivers(request):
     drivers = Driver.objects.all()
-    return render(request, 'taxi/drivers.html', {'drivers': drivers})
+    return render(request, "taxi/drivers.html",
+                  {"drivers": drivers})
