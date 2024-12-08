4c94bcc80b513439b9a6f67f5396c7782c4471ae
Yuriy Komar <omnigun85@gmail.com>
2024-12-08 02:11:24 +0200

    'Solution2'

--- taxi/views.py
+++ taxi/views.py
@@ -9,8 +9,8 @@ def index(request):
     num_cars = Car.objects.count()
 
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
     return render(request, "base.html", context=context)
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [
- BASE_DIR / "static",
+    BASE_DIR / "static",
 ]
 
 STATIC_URL = "static/"
