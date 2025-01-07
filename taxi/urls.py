728b6dafd280939910b69dca22eab35c6bf54cfb
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-01-07 02:27:10 +0100

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 
 
-from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
+from taxi.views import index
+from django.urls import path
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi")),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("", index, name="index"), ]
+
+app_name = "taxi"
