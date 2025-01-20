50516b7372219696ae181c4ff0c9dd6c8e71e1a4
4ikkamony <compbanderloga@gmail.com>
2025-01-20 17:23:26 +0200

    index view

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+from taxi.models import Driver, Manufacturer, Car
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_drivers = Driver.objects.count()
+    num_manufacturers: int = Manufacturer.objects.count()
+    num_cars: int = Car.objects.count()
+
+    return render(
+        request,
+        "taxi/index.html",
+        context={
+            "num_drivers": num_drivers,
+            "num_manufacturers": num_manufacturers,
+            "num_cars": num_cars
+        }
+    )
