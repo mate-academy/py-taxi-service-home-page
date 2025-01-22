016f779cd4727f3c3895425c2b633c887bc6ba4c
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-22 14:57:07 +0200

    taxi-solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Driver, Manufacturer, Car
+def index(request):
+    num_cars = Car.objects.count()
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
 
-# Create your views here.
+    context = {
+        "num_cars": num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+    }
+    return render(request, "taxi/index.html", context=context)
+
+def manufacturers_list(request):
+    return HttpResponse("Заглушка для списка производителей")
+
+
+def cars_list(request):
+    return HttpResponse("Заглушка для списка автомобилей")
+
+
+def drivers_list(request):
+    return HttpResponse("Заглушка для списка водителей")
+
+
