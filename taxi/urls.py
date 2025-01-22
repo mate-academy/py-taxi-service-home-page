038312d5bd7fa88f5be371b97f210ec25d441f5a
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-01-23 01:49:11 +0200

    Solution with flake fixed

--- taxi/urls.py
+++ taxi/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from taxi.views import index
 
 
+app_name = "taxi"
 urlpatterns = [
     path("", index, name="index"),
 ]
-
-app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from taxi.models import Manufacturer, Car, Driver
 
+
 def index(request: HttpRequest) -> HttpResponse:
     context = {
         "num_drivers": Driver.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
         "num_cars": Car.objects.count()
     }
+
     return render(
         request,
         "taxi/index.html",
