cbd89feed48ac7bee399bc411e30aa853c9e1224
ulichka <ulichka7615@gmail.com>
2025-01-28 21:53:50 +0100

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -10,18 +10,19 @@ from collections import Counter
 def index(request: HttpRequest) -> HttpResponse:
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     file_path = os.path.join(base_dir, "taxi_service_db_data.json")
-    with open(file_path, "r") as file:
-        data = json.load(file)
+    if os.path.exists(file_path):
+        with open(file_path, "r") as file:
+            data = json.load(file)
 
-    models_counter = Counter(item["model"] for item in data)
-    num_drivers = models_counter["taxi.driver"]
-    num_manufacturers = models_counter["taxi.manufacturer"]
-    num_cars = models_counter["taxi.car"]
+        models_counter = Counter(item["model"] for item in data)
+        num_drivers = models_counter["taxi.driver"]
+        num_manufacturers = models_counter["taxi.manufacturer"]
+        num_cars = models_counter["taxi.car"]
 
-    context = {
-        "num_cars": num_cars,
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-    }
+        context = {
+            "num_cars": num_cars,
+            "num_drivers": num_drivers,
+            "num_manufacturers": num_manufacturers,
+        }
 
     return render(request, "taxi/index.html", context=context)
