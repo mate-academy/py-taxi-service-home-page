cc607aba1638af0e7eacbdf3f619f2518a4b114f
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-02-05 15:16:49 +0100

    flake

--- taxi/views.py
+++ taxi/views.py
@@ -9,8 +9,8 @@ def index(request: HttpRequest) -> HttpResponse:
     num_manufacturers = Manufacturer.objects.all().count()
     num_cars = Car.objects.all().count()
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
     return render(request, "taxi/index.html", context=context)
