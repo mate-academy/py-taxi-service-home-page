37e4b56789bf5ff1ca64196ba81e2a96d9bd2850
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-02-19 11:36:58 +0200

    Fix

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import taxi.urls
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi"))
+    path("", include("taxi.urls", namespace="taxi_app"))
 ]
