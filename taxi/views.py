39622acef8fd0cdd818a39915634d748f488e7ed
Ron Phillips <mcspidy@gmail.com>
2025-02-21 10:59:33 -0600

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from taxi.models import Manufacturer, Driver, Car
+from typing import Any
 
-# Create your views here.
+
+def index(request) -> Any:
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+
+    return render(
+        request,
+        "taxi/index.html",
+        {
+            "num_drivers": num_drivers,
+            "num_manufacturers": num_manufacturers,
+            "num_cars": num_cars,
+        },
+    )
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Add this line to let Django know where to look for templates
+TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "templates")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
