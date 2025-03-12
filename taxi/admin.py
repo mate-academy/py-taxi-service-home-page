90e21569c432e04019abb4256294520e5e37010c
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-03-12 18:39:10 +0200

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,34 +1,44 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Driver, Car, Manufacturer
+
+from taxi.models import Car, Driver, Manufacturer
 
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ("license_number",)
+    list_display = UserAdmin.list_display + (
+        "license_number", "display_groups",
+    )
+    list_filter = ("groups", "is_superuser", "is_active",)
     fieldsets = UserAdmin.fieldsets + (
-        (("Additional info", {"fields": ("license_number",)}),)
+        ("Additional info", {"fields": ("license_number",)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (
-            (
-                "Additional info",
-                {
-                    "fields": (
-                        "first_name",
-                        "last_name",
-                        "license_number",
-                    )
-                },
-            ),
-        )
+            "Additional info", {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "license_number",
+                )
+            }
+        ),
     )
+
+    @admin.display(description="Groups")
+    def display_groups(self, obj: Driver) -> str:
+        return ", ".join([group.name for group in obj.groups.all()]) or "-"
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    search_fields = ("model",)
-    list_filter = ("manufacturer",)
+    list_display = ("model", "manufacturer",)
+    search_fields = ("model", "manufacturer__name",)
+    list_filter = ("model", "manufacturer",)
+    ordering = ("model",)
 
 
-admin.site.register(Manufacturer)
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("name", "country",)
+    ordering = ("name",)
