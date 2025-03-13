f383741208a63bf040b9bb8b3ea828def9496395
BohdanMT <metelua@gmail.com>
2025-03-13 23:51:51 +0200

    My solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    num_drivers = Driver.objects.all().count()
+    num_manufacturers = Manufacturer.objects.all().count()
+    num_cars = Car.objects.all().count()
+    context = {
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
+    }
+    return render(request, "taxi/index.html", context=context)
