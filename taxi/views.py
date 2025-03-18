d1c86a046436eeb032d8c9f1793c7431be870e26
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-03-18 11:17:53 +0200

    Fix taxi/views.py and taxi_service/urls.py

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Manufacturer, Driver, Car
 
+
 def index(request):
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
