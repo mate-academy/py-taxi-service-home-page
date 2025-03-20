8caf4c7903f7bac2f6996cf95e6fd5db1f42e489
Oleksandr Mazur <a9p9m9@gmail.com>
2025-03-20 10:44:41 +0200

    solution_1

--- taxi/views.py
+++ taxi/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 
-from taxi.models import *
+from taxi.models import Manufacturer, Driver, Car
 from django.shortcuts import render
+
 
 def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.all().count()
     num_manufacturers = Manufacturer.objects.all().count()
     num_cars = Car.objects.all().count()
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
 
     return render(request, "taxi/index.html", context=context)
