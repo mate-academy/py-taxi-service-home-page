63ed1cc5b898a43df19c8c84a8ce8e0926898501
bc2024-4 <bzabrodskij20@gmail.com>
2025-03-21 18:13:28 +0200

    fixed bugs

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.template.context_processors import static
+
 from django.urls import path, include
 
-from taxi_service import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="Home page"))
+    path("", include("taxi.urls", namespace="taxi"))
 ]
