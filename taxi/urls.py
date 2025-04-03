718bb66b11e7278af728e8d1a337c0e5c9ecab56
dima <giomozda74@gmail.com>
2025-04-03 23:51:00 +0300

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+from taxi.views import index
+app_name = "taxi"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi"))
+    path("", views.index, name="index")
 ]
