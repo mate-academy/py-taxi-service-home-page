7a2a4e55785e27dc810ee751a83b185e67d24e05
AndyStar <andy_star@ukr.net>
2025-04-03 17:41:56 +0300

    'Solution'

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("taxi/", include("taxi.urls", namespace='taxi')),
+    path("taxi/", include("taxi.urls", namespace="taxi")),
 ]
