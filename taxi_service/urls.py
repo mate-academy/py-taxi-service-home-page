5efd5003bf7d5e9f0dd0effe2ff7fb589386ad6f
dima <giomozda74@gmail.com>
2025-04-05 17:41:07 +0300

    final solution4

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-    path("__debug__/", include("debug_toolbar.urls"))
 ]
