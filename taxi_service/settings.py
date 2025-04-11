acd1faa949a18119aa8f5c92564cc2fc2cd56aa2
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-11 13:31:13 +0300

    settings.py

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,16 +43,6 @@ INSTALLED_APPS = [
     "taxi",
 ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
 ROOT_URLCONF = "taxi_service.urls"
 
 TEMPLATES = [
