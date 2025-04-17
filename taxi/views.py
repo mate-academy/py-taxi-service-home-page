6b9d0e5e67b056e7d043b38472552de0a6bae503
ViktorBl734 <harbourprogress@gmail.com>
2025-04-17 21:42:51 +0300

    Taxi service home page

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Driver, Manufacturer, Car
 
-# Create your views here.
+def index(request):
+    num_drivers = Driver.objects.all().count()
+    num_manufacturers = Manufacturer.objects.all().count()
+    num_cars = Car.objects.all().count()
+    return render(request, 'taxi/index.html', {
+        'num_drivers': num_drivers,
+        'num_manufacturers': num_manufacturers,
+        'num_cars': num_cars,
+    })
