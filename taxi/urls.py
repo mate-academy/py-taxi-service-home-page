13ff105dda9d902313d8ecf6b622f44d062cceb5
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-19 12:49:44 +0300

    formatted by flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from taxi.views import index
 
 
-urlpatterns = [
-    path("", index, name="index")
-]
+urlpatterns = [path("", index, name="index")]
 
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -7,14 +7,10 @@ from taxi.models import Driver, Manufacturer, Car
 def index(request) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_cars  = Car.objects.count()
+    num_cars = Car.objects.count()
     context = {
         "num_drivers": num_drivers,
         "num_manufacturers": num_manufacturers,
         "num_cars": num_cars,
     }
-    return render(
-        request,
-        "taxi/index.html",
-        context=context
-    )
+    return render(request, "taxi/index.html", context=context)
