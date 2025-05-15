ce3593a372dd3d211dc52b77a04eab3045a55b06
Nastya Kalinina <kalininan.kalinina@gmail.com>
2025-05-16 01:04:48 +0200

    Sol2

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from taxi.views import index
 from django.urls import path
 
+app_name = "taxi"
+
 urlpatterns = [
     path("", index, name="index"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-app_name = "taxi"
+
