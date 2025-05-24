0f830940e11ee7a8e05f8ea7a50009f99c2bc15c
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-05-24 15:52:10 +0200

    feat: craete taxi service home page

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, HttpRequest
+from .models import Driver, Manufacturer, Car
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "num_drivers": Driver.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count()
+    }
+
+    return render(request, "taxi/index.html", context)
