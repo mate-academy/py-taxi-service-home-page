94c0f81ff1172b7a55f5da2a1cd9a89ef226d0ac
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2025-06-10 20:44:08 +0300

    Taxi service home page

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -16,4 +17,4 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
-    drivers = models.ManyToManyField(Driver, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
