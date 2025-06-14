5ededb6c1dfc39643b5c1772459799838a2210ce
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-06-14 22:51:22 +0300

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import path
 from django.conf.urls.static import static
-from django.conf.urls.static import settings
+from django.conf import settings
 
 from taxi.views import index
 
