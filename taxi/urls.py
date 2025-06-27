5de87135d88aae1909bbc2ec8892ba278f059d4c
Alexey <alopar632@gmail.com>
2025-06-27 11:29:18 +0300

    Add static/css/styles.css and update sidebar links

--- taxi/urls.py
+++ taxi/urls.py
@@ -5,4 +5,7 @@ app_name = "taxi"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("manufacturers/", views.manufacturer_list, name="manufacturer-list"),
+    path("cars/", views.car_list, name="car-list"),
+    path("drivers/", views.driver_list, name="driver-list"),
 ]
