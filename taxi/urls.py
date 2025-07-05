abccd8801b6e624e5583f345ff7bd4484245683f
msi <lg.mobile0808@gmail.com>
2025-07-05 22:11:00 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "taxi"  # Add this line
+
 urlpatterns = [
     path("", views.index, name="index"),
+    # Add other paths as needed
 ]
