e26ed031f0429a9804fabf684bed16b17c4a95b0
MiKPo4eLiK <stassojcuk@gmail.com>
2025-07-13 13:05:05 +0300

    well done

--- taxi/views.py
+++ taxi/views.py
@@ -12,3 +12,15 @@ def index(request):
         "num_cars": num_cars,
     }
     return render(request, "taxi/index.html", context)
+
+def manufacturers_list(request):
+    manufacturers = Manufacturer.objects.all()
+    return render(request, "taxi/manufacturer_list.html", {"manufacturers": manufacturers})
+
+def cars_list(request):
+    cars = Car.objects.all()
+    return render(request, 'taxi/car_list.html', {'cars': cars})
+
+def drivers_list(request):
+    drivers = Driver.objects.all()
+    return render(request, 'taxi/driver_list.html', {'drivers': drivers})
