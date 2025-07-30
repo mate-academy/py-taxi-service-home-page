ff5d29075fe9abe12e74ab38d19cf6593cd7b6f8
kravetst <taraskravetc28@gmail.com>
2025-07-30 11:42:08 +0300

    Fix quotes style in taxi_service/urls.py to use double quotes

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -12,8 +12,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +64,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
