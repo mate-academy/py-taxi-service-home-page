cd57efcba65e0f05c22524e0f174f41577e8395d
Hetmarum <kpxmedet@gmail.com>
2025-08-10 20:42:32 +0300

    HttpResponse fix

--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
 
 from taxi.models import Driver, Manufacturer, Car
 
 
-def index(request: HttpRequest) -> HttpResponse:
+def index(request: HttpRequest) -> str:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
