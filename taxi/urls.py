7b02fe330a8739bedc038191611d5132eec4dffa
Otstoy <otstoy.sxe@gmail.com>
2025-08-11 09:32:18 +0300

    Removed not used imports.

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from taxi.views import index
@@ -7,6 +5,6 @@ from taxi.views import index
 
 urlpatterns = [
     path("", index, name="index")
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 app_name = "taxi"
