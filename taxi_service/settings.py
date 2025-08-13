da895c0e815a774029c3dfaad05dc6b14def41fe
Nik <your@mail.com>
2025-08-13 18:32:39 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_URL = "static/"
