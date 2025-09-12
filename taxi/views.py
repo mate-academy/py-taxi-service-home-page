e990512f92bf67993ecd62d0a623f08648a7a2f4
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-09-12 14:09:39 +0200

    Update sidebar, base.html and project settings

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .models import Manufacturer, Car
 
-# Create your views here.
+
+def index(request):
+    context = {
+        "num_drivers": get_user_model().objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_cars": Car.objects.count(),
+    }
+    return render(request, "taxi/index.html", context)
