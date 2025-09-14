56d7dc188ddb4adb68362cc13ce011289aa28ffa
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-09-14 16:24:47 +0300

    implemented py-taxi-service-home-page v3

--- taxi/views.py
+++ taxi/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
 
 from taxi.models import Driver, Manufacturer, Car
 
 
-# Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
+def index(request: HttpRequest):
     context = {
         "num_drivers": Driver.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
