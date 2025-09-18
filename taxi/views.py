40f75c6fcfa7d5d9f2299746e4fea9bfd408d602
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-09-18 10:12:38 +0300

    sol

--- taxi/views.py
+++ taxi/views.py
@@ -3,12 +3,12 @@ from .models import Manufacturer, Driver, Car
 
 
 def index(request):
-    num_drivers =  Driver.objects.count()
+    num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
     return render(request, "taxi/index.html", context=context)
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,7 +127,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
