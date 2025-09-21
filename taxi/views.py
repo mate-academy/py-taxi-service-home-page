2cc97537823952e2ed9bb4d4fdcf8e1092bf7ab3
Oleksandr Skoromnov <solalex005@gmail.com>
2025-09-21 22:52:22 +0300

    fix

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from taxi.models import Car, Manufacturer, Driver
+
+from taxi.models import Car, Driver, Manufacturer
 
 
 def index(request: HttpRequest) -> HttpResponse:
