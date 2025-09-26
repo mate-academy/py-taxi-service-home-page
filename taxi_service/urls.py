24a331a763290e5c9f0d85426b20c0acf9bbee02
Daniel Joseph <djoseph@envisagebr.com>
2025-09-26 17:53:17 +0300

    Sidebar with placeholder links; project static discovery; clean urls comment

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi")),  # <- namespace DEFINIDO
+    path("", include("taxi.urls", namespace="taxi")),
 ]
