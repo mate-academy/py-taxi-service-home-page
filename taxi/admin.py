dda223f6ad0477882724a9125d1e0346dffbe97b
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-10-02 17:49:15 +0300

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -29,6 +29,11 @@ class DriverAdmin(UserAdmin):
 class CarAdmin(admin.ModelAdmin):
     search_fields = ("model",)
     list_filter = ("manufacturer",)
+    list_display = ("model", "manufacturer",)
 
 
-admin.site.register(Manufacturer)
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    list_filter = ("country",)
+    search_fields = ("name",)
