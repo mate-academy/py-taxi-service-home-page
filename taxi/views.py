e09a889bd5eac79d5bc67029feb4451e336355de
mr_eugene <roberttimethone@gmail.com>
2025-10-14 23:11:12 +0200

    sol1

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Driver, Car, Manufacturer
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_drivers = Driver.objects.all()
+    num_manufacturers = Manufacturer.objects.all()
+    num_cars = Car.objects.all()
+    return HttpResponse("Hello, world!")
+
