7472717328689bddad6612a19abdd7e9afa8e3f1
mr_eugene <roberttimethone@gmail.com>
2025-10-15 21:29:14 +0200

    sol1

--- taxi/views.py
+++ taxi/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from taxi.models import Driver, Car, Manufacturer
+
+from taxi.models import Car, Manufacturer, Driver
 
 
 def index(request):
-    num_drivers = Driver.objects.all().count()
-    num_manufacturers = Manufacturer.objects.all().count()
-    num_cars = Car.objects.all().count()
-    context = {"num_drivers": num_drivers,
-               "num_manufacturers": num_manufacturers,
-               "num_cars": num_cars}
+    context = {"num_drivers": Driver.objects.all().count(),
+               "num_manufacturers": Manufacturer.objects.all().count(),
+               "num_cars": Car.objects.all().count()}
 
     return render(request, "taxi/index.html", context=context)
