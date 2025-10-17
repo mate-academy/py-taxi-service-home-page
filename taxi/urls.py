a02baeecf81490810cf66bf2ef5e06577a9fd9f7
Yaroslav <yaroslav.liubarets@icloud.com>
2025-10-17 10:45:41 +0000

    solution 2.3

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = "taxi"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include(("taxi.urls", "taxi"), namespace="taxi")),
+    path("", views.index, name="index"),
 ]
--- taxi/views.py
+++ taxi/views.py
@@ -1,14 +1,4 @@
-from django.shortcuts import render
-from taxi.models import Driver, Manufacturer, Car
-
+from django.http import HttpResponse
 
 def index(request):
-    num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_cars = Car.objects.count()
-    context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars,
-    }
-    return render(request, "taxi/index.html", context=context)
+    return HttpResponse("Welcome to the Taxi Service!")
