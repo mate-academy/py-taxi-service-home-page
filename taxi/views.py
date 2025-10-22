8f28d986f409b9178447888a3f79b0b5e850700c
vasylhladyshev404 <vasyl@nextgear.nl>
2025-10-22 13:41:14 +0200

    1

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Driver, Manufacturer, Car
 
-# Create your views here.
+
+def index(request):
+    num_drivers = Driver.objects.aggregate(count=Count("id"))["count"]
+    num_manufacturers = Manufacturer.objects.aggregate(count=Count("id"))["count"]
+    num_cars = Car.objects.aggregate(count=Count("id"))["count"]
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars
+    }
+    return render(request, "taxi/index.html", context)
