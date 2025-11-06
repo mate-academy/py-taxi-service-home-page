9c2e93b647ad43420ca0318fefda6f03011b9b68
Dmitrii Kozoriz <diluck2002@gmail.com>
2025-11-06 07:06:10 +0200

    Solution_3

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from taxi.views import index
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("taxi/", include("taxi.urls", namespace="taxi"))
+    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path("", index, name="index")
 ]
