989af84ab77ba28b89c1571a717ae78c59b1b9d4
Maksym Pakula <gargantadoto@gmail.com>
2025-12-03 23:42:56 +0200

    fixed some issues

--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 from taxi.models import Driver, Manufacturer, Car
 
 
-def index(request: HttpRequest) -> HttpResponse:
+def index(request):
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
