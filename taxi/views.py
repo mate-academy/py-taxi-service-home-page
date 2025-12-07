67c449783e9f248ab1c5a2abd3ff39208347db1c
Vadym Vynokur <king.krollie@gmail.com>
2025-12-07 19:41:58 +0200

    add sol

--- taxi/views.py
+++ taxi/views.py
@@ -6,9 +6,12 @@ from taxi.models import Manufacturer, Car
 
 
 def index(request):
+    num_drivers = get_user_model().objects.count()
+    num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
     context = {
-        "num_drivers": get_user_model().objects.count(),
-        "num_cars": Car.objects.count(),
-        "num_manufacturers": Manufacturer.objects.count(),
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
     }
     return render(request, "taxi/index.html", context=context)
