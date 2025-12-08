7899b6969352fd95c1a9917334bd6d0244b39791
Stanislav Kishechko <skishechko@gmail.com>
2025-12-08 19:56:15 +0200

    solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Driver, Car, Manufacturer
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
+    }
+
+    return render(request, "taxi/index.html", context=context)
