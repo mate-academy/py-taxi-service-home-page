843d937617a70cb45c54fad01ac6f128ead24462
Vadim <vadimchubok@gmail.com>
2025-12-10 16:39:10 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from.models import Driver, Manufacturer, Car
+from .models import Driver, Manufacturer, Car
 
 
 def index(request: HttpRequest) -> HttpResponse:
