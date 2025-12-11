80e8d62fe8f8b6d126c545cabbfdf59e4a50605d
VYGAKOOL <vygakool@gmail.com>
2025-12-11 10:32:23 +0200

    fix

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "taxi",
-    "taxi_service"
+    "taxi"
 ]
 
 MIDDLEWARE = [
