fb7ff887ef89613bb7a365f2466d849063c87546
Oleksii Shcherbak <qksardas001@mail.com>
2025-12-19 20:13:22 +0200

    bug fix

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import index
+
+from taxi.views import index
+
+
 
 app_name = "taxi"
 
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls")),
+    path("", include(("taxi.urls", "taxi"), namespace="taxi")),
+
 ]
