a467fa6064a5abd7e58ac9b9d468db8e54f61ebe
AKupriichuk <alexa05kup@gmail.com>
2026-01-07 22:39:01 +0200

    c2

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
+app_name = "taxi"
+
 urlpatterns = [
-# Порожні лапки '' означають головну сторінку http://127.0.0.1:8000/
-    path('', views.index, name='index'), #має вести до вьвс
+    path("", views.index, name="index"),
 ]
--- taxi/views.py
+++ taxi/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
-from taxi.models import Driver, Manufacturer, Car # Краще імпортувати моделі так
+
+from taxi.models import Driver, Manufacturer, Car
+
 
 def index(request):
-    # Рахуємо кількість об'єктів у базі
-    num_drivers = Driver.objects.count() # Можна писати відразу .count() без .all()
+    num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
 
-    # Створюємо словник з даними для шаблону
     context = {
-        'num_drivers': num_drivers,
-        'num_manufacturers': num_manufacturers,
-        'num_cars': num_cars,
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_cars": num_cars,
     }
 
-    return render(request, 'taxi/index.html', context=context)
+    return render(request, "taxi/index.html", context=context)
