4b65352fb6a00aedbdf255b285e0fc23841d343f
AKupriichuk <alexa05kup@gmail.com>
2026-01-07 23:12:32 +0200

    c4

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Замість просто include('taxi.urls', namespace="taxi-urls")
     path("", include("taxi.urls", namespace="taxi")),
 ]
+
