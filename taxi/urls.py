0371ef85b631743ee43d77764ca9f5e2a706652c
Hunach <sasha2001sek@gmail.com>
2026-01-23 22:59:40 +0200

    fix_quotes

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,13 +6,13 @@ from django.urls import path
 from taxi import views
 
 
-app_name = 'taxi'
+app_name = "taxi"
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('manufacturers/', views.manufacturer, name='manufacturers'),
-    path('cars/', views.car, name='cars'),
-    path('drivers/', views.driver, name='drivers'),
+    path("", views.index, name="index"),
+    path("manufacturers/", views.manufacturer, name="manufacturers"),
+    path("cars/", views.car, name="cars"),
+    path("drivers/", views.driver, name="drivers"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
