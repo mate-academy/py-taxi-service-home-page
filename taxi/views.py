990dd99709dd168686de647577dfa7aaa7d9f16c
Oleksandr <aleksandrpaskoo22@gmail.com>
2026-01-27 18:57:17 +0100

    implement

--- taxi/views.py
+++ taxi/views.py
@@ -5,11 +5,11 @@ from taxi.models import Driver, Manufacturer, Car
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    num_driver = Driver.objects.count()
+    num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
     context = {
-        "num_drivers": num_driver,
+        "num_drivers": num_drivers,
         "num_manufacturers": num_manufacturers,
         "num_cars": num_cars,
     }
