56f36f5da4ea2f8422766d6bae1f31750e434eef
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-01-27 09:04:17 -0300

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # CORREÇÃO E261:
+    path("admin/", admin.site.urls),
     path("taxi/", include("taxi.urls", namespace="taxi")),
 ]
